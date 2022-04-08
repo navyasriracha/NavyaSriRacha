7619d26f753da253e1a5d209ef3c58230a1eb557
Navya <103231589+navyasriracha@users.noreply.github.com>
2022-04-08 10:17:55 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+            let bio = document.createElement('p');
+            bio.textContent = "My name is navya"
+            let bodyelement = document.querySelector('p');
+            //bodyelement.appendChild(bio);
+
+            let script1 = document.querySelector('p');
+            bodyelement.insertBefore(bio,script1);
