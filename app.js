3de20604196d9da3057f4bfd48b03aebfc6c3db1
Navya <103231589+navyasriracha@users.noreply.github.com>
2022-04-09 21:49:08 +0530

    Update app.js
    set the background image

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let image_val = document.querySelector('img');
+image_val.style.height = `${window.innerHeight}px`; //setting the height of image to the height of screen
+image_val.style.width = `${window.innerWidth}px`;
 let output_value = document.getElementById("intro-output");
 let name_value = document.getElementById("name");
 let label = document.querySelector("label");
