98dbb306847a84858ed2f26d38ce1518a25fbb8d
Navya <103231589+navyasriracha@users.noreply.github.com>
2022-04-09 18:52:53 +0530

    Update app.js
    scripting for new changes

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-            let bio = document.createElement('p');
-            bio.textContent = "My name is navya"
-            let bodyelement = document.querySelector('p');
-            //bodyelement.appendChild(bio);
-
-            let script1 = document.querySelector('p');
-            bodyelement.insertBefore(bio,script1);
+let output_value = document.getElementById("intro-output");
+let name_value = document.getElementById("name");
+let label = document.querySelector("label");
+document.querySelector('form').addEventListener('submit',(e)=>{
+    e.preventDefault();
+    output_value.textContent = `Hey there! ${name_value.value}, If you wanna know me- you gotta wait. The page is under construction......`
+    label.textContent="";
+    
+});
